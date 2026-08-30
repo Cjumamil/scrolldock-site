7367950359cf1564133b6503c12fd7e5b3183d7b
COOP <89165831+Cjumamil@users.noreply.github.com>
2026-08-29 18:52:51 -0700

    Revert download URL to zip, matching currently published release

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const DOWNLOAD_URL = 'https://github.com/Cjumamil/scrolldock-site/releases/latest/download/ScrollDock.dmg'
+export const DOWNLOAD_URL = 'https://github.com/Cjumamil/scrolldock-site/releases/latest/download/ScrollDock.zip'
